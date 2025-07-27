9aa613a5de6f2497461ff680c9430c4c12abf420
yvantot <mcivannedalman@gmail.com>
2025-07-28 01:08:47 +0800

    qol

--- index.js
+++ index.js
@@ -20,12 +20,10 @@ class EntryLinks extends HTMLElement {
 			const [url, icon] = link.split(";");
 			if (url) {
 				const a = Utils.create_element("a", { href: url, target: "_blank" });
-				const img = Utils.create_element("img", { src: this.favicon_api + new URL(url).hostname, loading: "lazy" });
+				const img = Utils.create_element("img", { loading: "lazy" });
 
-				img.onerror = function () {
-					img.src = "./images/favicon-placeholder.png";
-					img.onerror = null;
-				};
+				if (icon) img.src = icon;
+				else img.src = this.favicon_api + new URL(url).hostname;
 
 				a.append(img);
 				container.append(a);
@@ -49,6 +47,7 @@ class Entry extends HTMLElement {
 		const name = this.getAttribute("app-name");
 		const screenshot = this.getAttribute("app-screenshot");
 		const description = this.getAttribute("app-description");
+		const screenshots = screenshot ? screenshot.split("|") : 0;
 
 		this.innerHTML = `
             <p class="huge-text" target="_blank">${title}</p>            
@@ -57,9 +56,31 @@ class Entry extends HTMLElement {
 					<p class="name">${name}</p>
 					<p class="description">${description}</p>    
 					<entry-links entry-links="${links}"></entry-link>
-				</div>          
-				<div class="screenshots">
-					<img loading="lazy" src="${screenshot}"/>
+				</div>				
+				<div class="screenshots" style="background-image: url('${screenshots[0]}')">
+					<div class="ss-count">
+						<span>1</span>
+						<span>/</span>
+						<span>${screenshots.length}</span>
+					</div>
+					<div class="ss-actions">
+						<button data-action="ss-previous" class="${screenshots.length > 1 ? "" : "no-display"}">
+							<svg>
+								<use href="#previous"></use>
+							</svg>
+						</button>
+						<button data-action="ss-zoom">
+							<svg>
+								<use href="#zoom"></use>
+							</svg>
+						</button>					
+						<button data-action="ss-next" class="${screenshots.length > 1 ? "" : "no-display"}">
+							<svg>
+								<use href="#next"></use>
+							</svg>
+						</button>
+					</div>
+					
 				</div>                      
             </div> 
         `.trim();
@@ -68,34 +89,102 @@ class Entry extends HTMLElement {
 
 class Index {
 	constructor() {
-		this.coloredHover = [];
+		this.coloredHover = null;
+		this.screenshotHover = null;
 	}
 
 	init() {
 		document.addEventListener("click", (event) => this.handleClick(event));
 		document.addEventListener("mouseover", (event) => this.handleMouseover(event));
+		document.addEventListener("mousemove", (event) => this.handleMousemove(event));
+	}
+
+	handleMousemove(event) {
+		if (this.screenshotHover) {
+			this.screenshotHover.style.backgroundSize = "contain";
+			this.screenshotHover.style.backgroundPositionY = "center";
+			this.screenshotHover.style.backgroundPositionX = "center";
+		}
+		const target = event.target.closest(".screenshots");
+		if (!target) return;
+		const is_zoom_on = target.dataset?.is_zoom_on === "true" ? true : false;
+		if (!is_zoom_on) return;
+
+		if (target.classList.contains("screenshots")) {
+			this.screenshotHover = target;
+
+			const rect = target.getBoundingClientRect();
+			const offset_y = Math.max(0, Math.min(100, ((event.y - rect.y) / rect.height) * 100));
+			const offset_x = Math.max(0, Math.min(100, ((event.x - rect.x) / rect.width) * 100));
+			const zoomFactor = 1000 / rect.width;
+			const bgWidth = rect.width * zoomFactor;
+
+			target.style.backgroundSize = bgWidth + "px";
+			target.style.backgroundPositionY = offset_y + "%";
+			target.style.backgroundPositionX = offset_x + "%";
+		}
 	}
 
 	handleMouseover(event) {
-		for (let e of this.coloredHover) {
-			if (e.classList.contains("click-entry")) continue;
-			e.style.backgroundColor = "";
+		if (this.coloredHover && !this.coloredHover.classList.contains("click-entry")) {
+			this.coloredHover.classList.remove("make-entry-hovered");
+			this.coloredHover.style.backgroundColor = "";
 		}
 
 		const target = event.target.closest("make-entry");
 		if (!target) return;
-		if (target.classList.contains("click-entry")) return;
-		if (target.style.backGroundColor) return;
 
-		const color = target.getAttribute("app-color") ?? "teal";
-		target.style.backgroundColor = color;
-		this.coloredHover.push(target);
+		if (target.classList.contains("click-entry")) return;
+
+		const bgcolor = target.getAttribute("app-color") ?? "teal";
+		target.style.backgroundColor = bgcolor;
+		target.classList.add("make-entry-hovered");
+		this.coloredHover = target;
 	}
 
 	handleClick(event) {
-		const target = event.target.closest("make-entry, entries-category");
+		const target = event.target.closest("make-entry, entries-category, button");
 		if (!target) return;
 		switch (target.nodeName) {
+			case "BUTTON": {
+				const make_entry = target.closest("make-entry");
+				const ss_container = target.closest(".screenshots");
+				const ss_count = ss_container.querySelector(".ss-count").children[0];
+				const screenshots = make_entry.getAttribute("app-screenshot").split("|");
+				let ss_index = ss_container.dataset?.ss_index ? parseInt(ss_container.dataset.ss_index) : 0;
+
+				switch (target.dataset.action) {
+					case "ss-next": {
+						ss_index++;
+						ss_index %= screenshots.length;
+						ss_count.innerText = ss_index + 1;
+						ss_container.dataset.ss_index = ss_index;
+						ss_container.style.backgroundImage = `url("${screenshots[ss_index]}")`;
+						break;
+					}
+					case "ss-previous": {
+						ss_index--;
+						ss_index = Math.abs(ss_index % screenshots.length);
+						ss_count.innerText = ss_index + 1;
+						ss_container.dataset.ss_index = ss_index;
+						ss_container.style.backgroundImage = `url("${screenshots[ss_index]}")`;
+						break;
+					}
+					case "ss-zoom": {
+						const is_zoom_on = ss_container.dataset.is_zoom_on === "true" ? true : false;
+
+						if (is_zoom_on) {
+							ss_container.classList.remove("zoom-on");
+						} else {
+							ss_container.classList.add("zoom-on");
+						}
+
+						ss_container.dataset.is_zoom_on = !is_zoom_on;
+						break;
+					}
+				}
+				break;
+			}
 			case "MAKE-ENTRY": {
 				target.classList.toggle("click-entry");
 				break;
@@ -103,10 +192,10 @@ class Index {
 			case "ENTRIES-CATEGORY": {
 				let next = target.nextElementSibling;
 				while (next) {
-					if (this.coloredHover.includes(next)) next.style.backgroundColor = "";
 					if (next.nodeName === "MAKE-ENTRY") {
-						next.classList.remove("click-entry");
 						next.classList.toggle("hide-entry");
+						next.classList.remove("click-entry");
+						next.style.backgroundColor = "var(--entry-bg)";
 						next = next.nextElementSibling;
 					} else {
 						break;
